99e5b122368c974b5ee138633cd980e2d8676373
Dilmurod D. Habibullayev <d.habibullayev@ung.uz>
2022-02-28 10:29:10 +0500

    ss

--- multicake/cakeapp/models.py
+++ multicake/cakeapp/models.py
@@ -1,3 +1,5 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -19,6 +21,7 @@ class Cake(models.Model):
     type = models.ForeignKey(CakeType, on_delete=models.CASCADE, verbose_name=_('Turi'))
     price_for_kg = models.FloatField(_('1 kg narxi'))
     min_amount_kg = models.PositiveIntegerField(_('Eng kam buyurtma miqdori (kg)'))
+    image = models.ImageField(_('Rasm'), upload_to='To\'rtlar')
 
 
     class  Meta:
@@ -28,4 +31,30 @@ class Cake(models.Model):
     def __str__(self) -> str:
         return self.name
 
+
+
+class Filling(models.Model):
+    name = models.CharField(_('Nomi'), max_length=100)
+    composition = models.CharField(_('Tarkibi'), max_length=255)
+    image = models.ImageField(_('Rasm'),blank=True, null=True, upload_to='Nachinka')
+
+
+    class Meta:
+        verbose_name = 'Nachinka'
+        verbose_name_plural = 'Nachinkalar'
+
+
+    def __str__(self) -> str:
+        return self.name
+
+    
+class Portfolio(models.Model):
+    image = models.ImageField(_('Rasm'), upload_to='Biznig ishlar')
+
+
+    class Meta:
+        verbose_name = 'Bizning ish'
+        verbose_name_plural = 'Bizning ishlar'
+
+    
 

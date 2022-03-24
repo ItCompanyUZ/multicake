b990332380890089c886647db3d8e0f5f458d91a
ramziddin1671 <ramziddin_jurabek.uz@mail.ru>
2022-03-24 21:19:48 +0500

    iconca

--- multicake/multicake/settings.py
+++ multicake/multicake/settings.py
@@ -81,24 +81,24 @@ WSGI_APPLICATION = 'multicake.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'multicake',
-#         'USER': 'multicake',
-#         'PASSWORD': 'multicake$$$',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'multicake',
+        'USER': 'multicake',
+        'PASSWORD': 'multicake$$$',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
